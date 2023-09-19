5a0967874e2d123972e6c401c76ce39e09f6e77e
hui1601 <hui1601@naver.com>
2023-09-19 17:56:48 +0900

    Fix Feels like Translation

--- src/uiCurrent_w.py
+++ src/uiCurrent_w.py
@@ -71,7 +71,7 @@ def current_weather(main_window,upper_row,data,air_pollution_data):
     temp_label.set_css_classes(['temp_label'])
     temp_box_l.append(temp_label)
 
-    feels_like_label = Gtk.Label(label=_(f"Feels like {data['main']['feels_like']:.1f}{measurements[measurement_type]['temp_unit']}"))
+    feels_like_label = Gtk.Label(label=_("Feels like {0:.1f}{1}").format(data['main']['feels_like'], measurements[measurement_type]['temp_unit']))
     feels_like_label.set_halign(Gtk.Align.START)
     feels_like_label.set_margin_start(5)
     feels_like_label.set_css_classes(['secondary-light','f-mlg'])
