9ae59ab1299023ae228c581c1b9fec1fe0b3fc2a
Amit Chaudhary <amitchaudhary0539@outlook.com>
2024-01-16 01:45:12 +0530

    release v1.0.1

--- src/windowAbout.py
+++ src/windowAbout.py
@@ -1,17 +1,17 @@
 import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw,Gio,GLib
+from gi.repository import Gtk, Adw
 
 def AboutWindow(parent,*args):
         dialog = Adw.AboutWindow.new()
         dialog.set_application_name(_("Weather"))
         dialog.set_application_icon("io.github.amit9838.weather")
-        dialog.set_version("1.0.0")
+        dialog.set_version("1.0.1")
         dialog.set_developer_name("Amit Chaudhary")
         dialog.set_license_type(Gtk.License(Gtk.License.GPL_3_0))
         dialog.set_comments(_("Beautiful and light weight weather app build using Gtk and python"))
-        dialog.set_website("https://github.com/amit9838/weather")
+        dialog.set_website("https://amit9838.github.io/weather/")
         dialog.set_issue_url("https://github.com/amit9838/weather/issues")
         # dialog.add_credit_section("Contributors", ["name url"])
         dialog.set_copyright(_("Copyright © 2023 Weather Developers"))
