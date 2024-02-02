80b44856293bec6239ec631cdc1c908341a239b1
Himadri Sekhar Basu <41947504+hsbasu@users.noreply.github.com>
2024-02-02 19:25:17 +0530

    Fix packaging (#58)
    * Fix snap packaging
    
    - Fixed missing artifacts in snap version
    - Use meson version >=0.61 for compatibility
      in ubuntu 22.04 (current version 0.61.2)
    - Update project version to 1.0.1
    
    * fixes as needed
    
    * remove the conditions based on packaging format
    
    ---------
    
    Co-authored-by: সৌম্যদীপ ঘোষ <72045785+soumyaDghosh@users.noreply.github.com>

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,7 @@
 import os
 
-icon_loc = "/app/share/icons/hicolor/scalable/weather_icons/"
 
-if  os.getenv('SNAP'):
-    icon_loc = f"{os.getenv('SNAP')}/usr/share/icons/hicolor/scalable/weather_icons/"
+icon_loc = "@icon_location@/share/icons/hicolor/scalable/weather_icons/"
 
 icons = {
     "0": icon_loc + "clear-day.svg",
