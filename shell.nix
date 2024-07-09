bb3faf70e9d432e92ba3e17438455de8352693a1
drunkbit <174365495+drunkbit@users.noreply.github.com>
2024-07-09 22:13:46 +0200

    optimize nix shell

--- shell.nix
+++ shell.nix
@@ -4,17 +4,19 @@ let
   # use unstable branch for latest versions
   nixpkgs-src = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
 
-  pkgs = import nixpkgs-src { config = { allowUnfree = false; }; };
+  # allow unfree packages or not
+  pkgs = import nixpkgs-src { config = { allowUnfree = true; }; };
 
   # python version
   myPython = pkgs.python3;
 
+  # python packages
   pythonWithPkgs = myPython.withPackages (pythonPkgs: with pythonPkgs; [
-    #black
+    # note: add python packages into requirements.txt file
+    # note: if vscode does not detect python module from requirements.txt, change python interpreter (bottom right corner)
+    black
     ipython
     pip
-    pygobject3
-    requests
     setuptools
     virtualenvwrapper
     wheel
@@ -22,13 +24,13 @@ let
 
   shell = pkgs.mkShell {
     buildInputs = with pkgs; [
-      # python version with packages from above
+      # python version with python packages from above
       pythonWithPkgs
 
       # formatter for this file
       nixpkgs-fmt
 
-      # meson
+      # need to run this project
       appstream-glib
       cmake
       desktop-file-utils
@@ -38,16 +40,16 @@ let
       gtk4
       libglibutil
       librsvg
-      pkg-config
       meson
       ninja
+      pkg-config
     ];
 
     shellHook = ''
-      # allow the use of wheels
+      # allow use of wheels
       SOURCE_DATE_EPOCH=$(date +%s)
 
-      # setup the virtual environment if it does not already exist
+      # setup virtual environment if it does not already exist
       VENV=.venv
       if test ! -d $VENV; then
         virtualenv $VENV
@@ -55,8 +57,8 @@ let
       source ./$VENV/bin/activate
       export PYTHONPATH=`pwd`/$VENV/${myPython.sitePackages}/:$PYTHONPATH
 
-      pip install --disable-pip-version-check pygobject
-      pip install --disable-pip-version-check requests
+      # install packages from requirements.txt via pip
+      pip install --disable-pip-version-check -r requirements.txt
 
       # path to compiled schemas: https://stackoverflow.com/questions/28953925/glib-gio-error-no-gsettings-schemas-are-installed-on-the-system
       export XDG_DATA_DIRS=`pwd`/compiledir/share/
