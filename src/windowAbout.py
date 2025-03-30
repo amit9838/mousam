105851320bc7e13a65caa8828824748814b6f1e2
Amit Chaudhary <amitchaudhary0539@outlook.com>
2025-03-30 18:45:15 +0530

    use aboutDiaglog instead of aboutWindow

--- src/windowAbout.py
+++ src/windowAbout.py
@@ -2,24 +2,24 @@ import gi
 from gi.repository import Gtk, Adw
 from gettext import gettext as _, pgettext as C_
 
-
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 
-def AboutWindow(parent,*args):
-    dialog = Adw.AboutWindow.new()
-    dialog.set_application_name("Mousam")
-    dialog.set_application_icon("io.github.amit9838.mousam")
-    dialog.set_version("1.4.0")
-    dialog.set_developer_name("Amit Chaudhary")
-    dialog.set_license_type(Gtk.License(Gtk.License.GPL_3_0))
-    dialog.set_comments(_("Weather at a glance"))
-    dialog.set_website("https://amit9838.github.io/mousam/")
-    dialog.set_issue_url("https://github.com/amit9838/mousam/issues")
-    # dialog.add_credit_section("Contributors", ["name url"])
-    dialog.set_copyright(_("Copyright © 2024 Mousam Developers"))
-    dialog.set_developers(["Amit Chaudhary"])
-    # Translators: Please enter your credits here. (format: "Name https://example.com" or "Name <email@example.com>", no quotes)
-    dialog.set_translator_credits(_("translator_credits"))
-    dialog.set_transient_for(parent)
-    dialog.present()
+
+"""Present the app's about dialog."""
+def AboutWindow(*args):
+    about_window = Adw.AboutDialog(
+        application_icon="io.github.amit9838.mousam",
+        application_name="Mousam",
+        comments = _("Weather at a glance"),
+        developer_name="Amit Chaudhary",
+        developers= ["Amit Chaudhary"],
+        issue_url="https://github.com/amit9838/mousam/issues",
+        version="1.4.0",
+        website="https://amit9838.github.io/mousam/",
+        copyright=_("Copyright © 2024 Mousam Developers"),
+        license_type=Gtk.License(Gtk.License.GPL_3_0),
+        translator_credits=_("translator-credits"),
+    )
+    
+    about_window.present()
